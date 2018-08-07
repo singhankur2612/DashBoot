aadef6ee7b2d674de42ea4167746c4398dbc7bb6
singhankur2612 <singhankur2612@gmail.com>
2018-08-07 17:13:03 +0530

    Add files via upload

---
+++ src/main/java/com/boot/DashBootApplication.java
@@ -0,0 +1,12 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashBootApplication.class, args);
+	}
+}
